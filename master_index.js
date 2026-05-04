5d4514b2883ba886c341b1dcbe5af4944670ffc2
abrahymbaqr678-collab <abrahymbaqr678@gmail.com>
2026-05-04 09:20:24 +0300

    Update master_index.js

--- master_index.js
+++ master_index.js
@@ -1,15 +1,11 @@
-// استدعاء ملفات البيانات مباشرة
-function loadExternalData() {
-    const scripts = [
-        "https://raw.githubusercontent.com/abrahymbaqr678-collab/Repository-name/main/data_part1.js"
-    ];
+// مصفوفة الروابط التي سيتم تحميلها كـ Scripts
+var data_sources = [
+  "https://raw.githubusercontent.com/abrahymbaqr678-collab/Repository-name/main/data_part1.js"
+];
 
-    scripts.forEach(url => {
-        let script = document.createElement('script');
-        script.src = url;
-        script.async = false; // لضمان الترتيب والسرعة
-        document.head.appendChild(script);
-    });
-}
-
-loadExternalData();
+// دالة التحميل التلقائي
+data_sources.forEach(function(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.head.appendChild(script);
+});
